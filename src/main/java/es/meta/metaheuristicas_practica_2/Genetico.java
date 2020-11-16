179951c38009f9ae2d3703b489b0f3861fb8bbd8
Andrés Rojas Ortega <aro00015@red.ujaen.es>
2020-11-16 22:53:20 +0100

    Genetico: Reparar, FuncionSolucion, CalcularAportes, CalcularMayorAporte, Mutar, ReemplazarElite código añadido y comentado de la rama david

--- src/main/java/es/meta/metaheuristicas_practica_2/Genetico.java
+++ src/main/java/es/meta/metaheuristicas_practica_2/Genetico.java
@@ -14,6 +14,12 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static jdk.internal.org.jline.utils.ExecHelper.exec;
 
 /**
  * @brief Clase que implementa toda la funcionalidad del algoritmo genético
@@ -23,6 +29,87 @@ import java.util.Set;
  * @date 13/11/2020
  */
 public class Genetico {
+
+    /**
+     * @brief Clase que implementa la funcionalidad de un hilo para poder
+     * reparar una sección de la población
+     * @class CostTask
+     * @author David Díaz Jiménez
+     * @author Andrés Rojas Ortega
+     * @date 16/11/2020
+     */
+    private class CostTask implements Callable<ArrayList<Cromosomas>> {
+
+        ///Atributos de la clase:
+        private ArrayList<Cromosomas> Cromosomas;///<Conjunto de individuos
+        private final int empieza;///<Posición del primer individuo
+        private final int termina;///<Posición del último individuo
+
+        /**
+         * @brief Constructor parametrizado de la clase.
+         * @author David Díaz Jiménez
+         * @author Andrés Rojas Ortega
+         * @date 16/11/2020
+         * @param cromosomas ArrayList<Cromosomas>
+         * @param empie int
+         * @param termi int
+         */
+        public CostTask(ArrayList<Cromosomas> cromosomas, int empie, int termi) {
+            Cromosomas = new ArrayList<>(cromosomas);
+            empieza = empie;
+            termina = termi;
+
+        }
+
+        @Override
+        public ArrayList<Cromosomas> call() throws Exception {
+            int numeroGenes = _archivoDatos.getTama_Solucion();
+
+            ArrayList<Cromosomas> cromosomaReparado = new ArrayList<>();
+            int i = 0;
+            for (Cromosomas Cromosoma : Cromosomas) {
+
+                if (i >= empieza && i <= termina) {
+
+                    Set<Integer> cromosoma = Cromosoma.getCromosoma();
+
+                    if (cromosoma.size() != numeroGenes) {
+
+                        if (cromosoma.size() < numeroGenes) {
+
+                            while (cromosoma.size() != numeroGenes) {
+                                //Calcular mejor coste como en Greedy      
+                                int gen = CalcularMayorAporte(cromosoma);
+                                cromosoma.add(gen);
+                            }
+
+                            Cromosoma.setCromosoma(cromosoma);
+
+                            cromosomaReparado.add(new Cromosomas(Cromosoma));
+
+                        } else {
+
+                            while (cromosoma.size() != numeroGenes) {
+                                //Quitar los que menos aportan
+                                int elemento = CalcularMenorAporte(cromosoma);
+                                cromosoma.remove(elemento);
+                            }
+
+                            Cromosoma.setCromosoma(cromosoma);
+
+                            cromosomaReparado.add(new Cromosomas(Cromosoma));
+                        }
+                    } else {
+                        cromosomaReparado.add(new Cromosomas(Cromosoma));
+                    }
+                    i++;
+                } else {
+                    i++;
+                }
+            }
+            return cromosomaReparado;
+        }
+    }
 
     ///Atributos de la clase:
     Archivo _archivoDatos;///<Contiene los datos del problema
@@ -56,10 +143,10 @@ public class Genetico {
      * @param OperadorMPX boolean
      * @param probReini float
      * @apram probMutacion float
-     * 
+     *
      */
     public Genetico(Archivo _archivoDatos, GestorLog gestor, int evaluaciones, int Elitismo, boolean OperadorMPX, float probReini,
-             float probMutacion) {
+            float probMutacion) {
         this._archivoDatos = _archivoDatos;
         this._gestor = gestor;
         this._elitismo = Elitismo;
@@ -87,7 +174,7 @@ public class Genetico {
      * @author David Díaz Jiménez
      * @author Andrés Rojas Ortega
      * @date 13/11/2020
-     * @param alea Random_p Semilla utilizada para generar números 
+     * @param alea Random_p Semilla utilizada para generar números
      * pseudoaleatorios
      */
     void generarCromosomasIniciales(Random_p alea) {
@@ -103,10 +190,10 @@ public class Genetico {
                     cromosoma.add(alelo);
                 }
             }
-            _cromosomas.add(new Cromosomas(cromosoma, 0.0f,true));
+            _cromosomas.add(new Cromosomas(cromosoma, 0.0f, true));
         }
     }
-    
+
     /**
      * @brief Función encargada de realizar la reproducción y generación de la
      * siguiente generación del algoritmo genético.
@@ -120,15 +207,15 @@ public class Genetico {
         if (_operadorMPX == true) {
 
             operadorCruceMPX(alea);
-            
+
         } else {
 
             operadorCruce2puntos(alea);
         }
-        
+
         _cromosomasPadre.clear();
     }
-    
+
     /**
      * @brief Selecciona el conjunto de individuos que se reproduciran en la
      * generación actual aplicando torneo binario.
@@ -154,10 +241,10 @@ public class Genetico {
                 _cromosomasPadre.add(new Cromosomas(_cromosomas.get(candidato2)));;
             }
         }
-        
+
         _cromosomas.clear();
     }
-    
+
     /**
      * @brief Realiza el cruce de la poblacion utilizando el operador de cruce
      * en dos puntos.
@@ -166,10 +253,10 @@ public class Genetico {
      * @date 15/11/2020
      * @param alea Random_p Utilizado para generar números pseudoaleatorios
      */
-    private void operadorCruce2puntos(Random_p alea){
+    private void operadorCruce2puntos(Random_p alea) {
 
         //Recorremos el vector por parejas de padres
-        for (int i = 0; i < _numeroCromosomas-1; i += 2) {
+        for (int i = 0; i < _numeroCromosomas - 1; i += 2) {
 
             //Comprobamos si debemos realizar el cruce para la preja
             float probRepro = (float) alea.Randfloat(0, 1);
@@ -184,13 +271,13 @@ public class Genetico {
                 int crosspoint2 = alea.Randint(1, _archivoDatos.getTama_Solucion() - 2);
 
                 //Comprobamos que los puntos de corte no coincidan
-                while(crosspoint1 == crosspoint2){
+                while (crosspoint1 == crosspoint2) {
                     crosspoint2 = alea.Randint(1, _archivoDatos.getTama_Solucion() - 2);
                 }
 
                 //Realizamos el cruce
                 Iterator<Integer> iterator = _cromosomasPadre.get(i).getCromosoma().iterator();
-                Iterator<Integer> iterator1 = _cromosomasPadre.get(i+1).getCromosoma().iterator();
+                Iterator<Integer> iterator1 = _cromosomasPadre.get(i + 1).getCromosoma().iterator();
 
                 int length = _archivoDatos.getTama_Solucion();
                 for (int j = 0; j < length; j++) {
@@ -205,21 +292,223 @@ public class Genetico {
                 }
 
                 //Almacenamos los hijos generados
-                _cromosomasHijo.add(new Cromosomas(hijo1,0.0f,true));
-                _cromosomasHijo.add(new Cromosomas(hijo2,0.0f,true));
+                _cromosomasHijo.add(new Cromosomas(hijo1, 0.0f, true));
+                _cromosomasHijo.add(new Cromosomas(hijo2, 0.0f, true));
 
             } else {
 
                 //Guardamos los padres sin cruzar como resultado del cruce
-                
                 Cromosomas hijo1 = _cromosomasPadre.get(i);
-                Cromosomas hijo2 = _cromosomasPadre.get(i+1);
+                Cromosomas hijo2 = _cromosomasPadre.get(i + 1);
 
-                _cromosomasHijo.add(new Cromosomas(hijo1.getCromosoma(),hijo1.getContribucion()));
-                _cromosomasHijo.add(new Cromosomas(hijo2.getCromosoma(),hijo2.getContribucion()));
+                _cromosomasHijo.add(new Cromosomas(hijo1.getCromosoma(), hijo1.getContribucion()));
+                _cromosomasHijo.add(new Cromosomas(hijo2.getCromosoma(), hijo2.getContribucion()));
 
             }
         }
+    }
+
+    /**
+     * @brief Repara los cromosomas que no son una solución válida haciendo uso
+     * de concurrencia.
+     * @author David Díaz Jiménez
+     * @author Andrés Rojas Ortega
+     * @date 16/11/2020
+     */
+    private void repararConcurrente() {
+
+        Future<ArrayList<Cromosomas>> future = null;
+        Future<ArrayList<Cromosomas>> future2 = null;
+
+        int tam = ((_numeroCromosomas) / 2) - 1;
+        future = exec.submit(new CostTask(_cromosomasHijo, 0, tam));
+        future2 = exec.submit(new CostTask(_cromosomasHijo, tam + 1, _numeroCromosomas - 1));
+
+        try {
+
+            _cromosomasHijo.clear();
+            for (Cromosomas cromo : future.get()) {
+                _cromosomasHijo.add(cromo);
+            }
+            for (Cromosomas cromo : future2.get()) {
+                _cromosomasHijo.add(cromo);
+            }
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Genetico.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            Logger.getLogger(Genetico.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * @brief Calcula el gen que más coste aporta al individuo si lo añadimos
+     * @author David Díaz Jiménez
+     * @author Andrés Rojas Ortega
+     * @date 16/11/2020
+     * @param cromosoma Setz<Integer> Material genético del individuo
+     * @return elemento int El elemento que más aporta si lo añadimos al materal
+     * genético
+     */
+    private int CalcularMayorAporte(Set<Integer> cromosoma) {                   //Revisar
+
+        float mejor = 0.0f;
+        int elemento = 0;
+
+        HashSet<Integer> cromosomaReparado = new HashSet<>(cromosoma);
+        int tamMatrix = _archivoDatos.getTama_Matriz();
+
+        for (int i = 0; i < tamMatrix; i++) {
+
+            if (!cromosomaReparado.contains(i)) {
+
+                float costeMas = 0.0f;
+                float coste;
+                Iterator<Integer> iterator = cromosomaReparado.iterator();
+
+                while (iterator.hasNext()) {
+                    int k = iterator.next();
+                    costeMas += _archivoDatos.getMatriz()[k][i];
+                }
+
+                coste = costeMas;
+
+                if (coste > mejor) {
+                    mejor = coste;
+                    elemento = i;
+                }
+
+            }
+        }
+
+        return elemento;
+    }
+    
+    /**
+     * @brief Calcula el gen del individuo que menos coste aporta.
+     * @author David Díaz Jiménez
+     * @author Andrés Rojas Ortega
+     * @date 16/11/2020
+     * @param cromosoma Set<Integer> Material genético del individuo
+     * @return elemenor int El elemento que menos coste aporta
+     */
+    private int CalcularMenorAporte(Set<Integer> cromosoma) {
+
+        float aporte = 0.0f;
+        Iterator<Integer> iterator = cromosoma.iterator();
+        float menorAporte = Float.MAX_VALUE;
+        int elemenor = -1;
+
+        while (iterator.hasNext()) {
+
+            Iterator<Integer> iterator2 = cromosoma.iterator();
+            int i = iterator.next();
+
+            while (iterator2.hasNext()) {
+
+                int j = iterator2.next();
+                aporte += _archivoDatos.getMatriz()[i][j];
+
+            }
+
+            if (aporte < menorAporte) {
+                menorAporte = aporte;
+                elemenor = i;
+            }
+
+            aporte = 0.0f;
+        }
+
+        return elemenor;
+    }
+    
+    /**
+     * @brief Operador de mutación.
+     * @author David Díaz Jiménez
+     * @author Andrés Rojas Ortega
+     * @date 16/11/2020
+     * @param ale Random_p Semilla aleatoria para generar números
+     */
+    private void operadorMutación(Random_p ale) {
+
+        for (Cromosomas Cromosoma : _cromosomasHijo) {
+
+            ArrayList<Integer> alelosMutados = new ArrayList<>();
+            ArrayList<Integer> alelosNuevos = new ArrayList<>();
+
+            Set<Integer> cromosoma = Cromosoma.getCromosoma();
+
+            for (Integer gen : cromosoma) {
+
+                double probabilidad = ale.Randfloat(0, 1);
+
+                if (probabilidad < _probMutacion) {
+
+                    Cromosoma.setRecalcular(true);
+                    alelosMutados.add(gen);
+
+                    boolean reemplazo = false;
+
+                    while (reemplazo == false) {
+
+                        int generado = ale.Randint(0, _archivoDatos.getTama_Matriz() - 1);
+
+                        if (!cromosoma.contains(generado) && !alelosNuevos.contains(generado)) {
+                            alelosNuevos.add(generado);
+                            reemplazo = true;
+                        }
+                    }
+                }
+            }
+
+            while (!alelosMutados.isEmpty() || !alelosNuevos.isEmpty()) {
+                cromosoma.remove(alelosMutados.remove(0));
+                cromosoma.add(alelosNuevos.remove(0));
+            }
+
+            Cromosoma.setCromosoma(cromosoma);
+        }
+    }
+    
+    /**
+     * @brief Operador de elitismo
+     * @author David Díaz Jiménez
+     * @author Andrés Rojas Ortega
+     * @date 16/11/2020
+     */
+    private void operadorElitismo() {
+
+        if (_generacional == false) {
+            for (Cromosomas Mejores : cromosomasElite) {
+                _cromosomasHijo.add(new Cromosomas(Mejores));
+            }
+        }
+
+        Collections.sort(_cromosomasHijo);
+
+        while (_cromosomasHijo.size() > _numeroCromosomas) {
+            _cromosomasHijo.remove(0);
+        }
+
+        int i = 1;
+        for (Cromosomas elite : cromosomasElite) {
+            elite.setCromosoma(_cromosomasHijo.get(_numeroCromosomas - i).getCromosoma());
+            elite.setContribucion(_cromosomasHijo.get(_numeroCromosomas - i).getContribucion());
+            i++;
+        }
+
+        _cromosomas.clear();
+        _cromosomas = new ArrayList<>(_cromosomasHijo);
+
+        _cromosomasHijo.clear();
+        _cromosomasPadre.clear();
+
+        if (cromosomasElite.get(0).getContribucion() > _mejorCromosoma.getContribucion()) {
+            _mejorCromosoma = new Cromosomas(cromosomasElite.get(0));
+        }
+
+        registroElites();
     }
 
     void PresentarResultados() {
